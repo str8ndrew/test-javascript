1661edc911582504c3c1852dfc25ddfe05aca275
str8ndrew <str8ndrew@users.noreply.github.com>
2024-01-12 20:48:06 +0100

    add js switch

---
+++ js/switch.js
@@ -0,0 +1,20 @@
+let word = 'Some';
+
+switch (word) {
+    case "new":
+        console.log("Var is new");
+        break;
+    case 5:
+        console.log("Var is 5");
+        break;
+    case "Some":
+        console.log("Var is some");
+        break;
+    case 15:
+        console.log("Var is 15");
+        break;
+    default:
+        console.log("Var is unknown");
+}
+
+
