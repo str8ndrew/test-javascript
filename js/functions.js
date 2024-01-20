aed21f90d79e6005206ba53483139935e9ec2f3b
str8ndrew <str8ndrew@users.noreply.github.com>
2024-01-20 18:05:03 +0100

    setap

--- js/functions.js
+++ js/functions.js
@@ -138,3 +138,18 @@ console.log(result);
 const sub = "vip";
 const canAcces = sub === "pro" || sub === "vip";
 console.log(canAcces);
+
+
+
+/* Напиши скрипт, який перевіряє можливість відкрити чат з користувачем
+Для цього користувач має бути: 
+- другом
+- онлайн
+- без режима не турбувати 
+*/
+const isOnline = true;
+const isFriend = true;
+const isDnd = true;
+
+const canOpenChat = isOnline && isFriend && !isDnd;
+console.log("Можна відкрити чат?", canOpenChat);
