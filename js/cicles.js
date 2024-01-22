d73a656860832190d2292b74217721f42216a4ed
str8ndrew <str8ndrew@users.noreply.github.com>
2024-01-22 20:09:35 +0100

    add methods and cicles

--- js/cicles.js
+++ js/cicles.js
@@ -15,3 +15,39 @@ for (let i = 10; i > 0; i -= 1) {
 for (let i = 2; i <= 30; i += 2) {
     console.log(i);
 }
+
+
+/* while - використовується тоді, коли ми не знаємо точну кількість ітерацій,
+але знаємо умову до яких пір повинен виконуватися цикл
+*/
+
+let counter = 0;
+while (counter < 10) {
+    console.log(counter);
+    counter += 1;
+}
+
+console.log("While examples");
+
+const correcrPass = "qwerty";
+let userPass = prompt("Enter your password");
+let attempts = 3;
+while (userPass !== correcrPass && attempts > 2) {
+    userPass = prompt("Your password isnt correct. Try again!");
+    attempts -= 1;
+}
+
+console.log(userPass);
+
+
+let clientCounter = 18;
+const maxClients = 25;
+while (clientCounter < maxClients) {
+    console.log(clientCounter);
+    clientCounter += 1;
+}
+
+
+
+
+console.log("While examples");
