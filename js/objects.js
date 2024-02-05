5f33c2ecc6c4572cd9636beb56ca7bcc9c25e6bc
str8ndrew <str8ndrew@users.noreply.github.com>
2024-02-05 10:42:15 +0100

    change object

--- js/objects.js
+++ js/objects.js
@@ -1,11 +1,23 @@
-const book = {
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  rating: 8.38,
-};
-const keys = Object.keys(book);
+// const book = {
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
 
+// for (const key of keys) {
+//   console.log(key); // Ключ
+//   console.log(book[key]); // Значення властивості
+// }
+
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = Object.keys(apartment);
 for (const key of keys) {
-  console.log(key); // Ключ
-  console.log(book[key]); // Значення властивості
+  console.log(apartment[key]);
 }
+const values = ["Spacious apartment in the city center", 4, 2153];
